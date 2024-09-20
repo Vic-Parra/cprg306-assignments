70a50565d5eb5aa15424a15ca386174a18aae471
Vic Parra <152223182+Vic-Parra@users.noreply.github.com>
2024-09-20 13:47:35 -0600

    Week 3 Assignment Started
    - created week-3 folder
      - created item.js
      - created item-list.js
      - created page.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,4 +1,4 @@
-// SECOND PAGE
+// WEEK 2 PAGE
 
 // imports
 import Link from "next/link";
@@ -15,6 +15,7 @@ export default function Page() {
         <div> 
         <Link href="../" className= "text-rose-200 hover:text-rose-500 p-3">← Back</Link>
         <Link href="#" className= "text-rose-200 hover:text-rose-500 p-3">Next →</Link>
+        <Link href="./page.js/" className= "text-rose-200 hover:text-rose-500 p-3">Home</Link>
         </div>
       </main>
     );
