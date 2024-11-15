17398ec6093d98a50c9e25f8b1b59911c3461db4
Vic Parra <152223182+Vic-Parra@users.noreply.github.com>
2024-11-15 13:23:10 -0700

    Worked on Week 5 Assignment
    still need to fix some weird bugs

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -4,8 +4,16 @@ import { useState } from "react";
 
 export default function NewItem (){
 
+    // quantity use state 
     const [quantity, setQuantity] = useState(1);
 
+    // name use state
+    const [name, setName] = useState("");
+
+    // category use state 
+    const [category, setCategory] = useState("produce");
+
+    // Counter 
     let btnDisabled = false;
     if (quantity == 1){
         btnDisabled = true;
@@ -20,12 +28,63 @@ export default function NewItem (){
         }
     }
 
-    const btnStyle = "bg-slate-500 text-white rounded py-2 px-4 mt-5 hover:bg-slate-700 active:bg-blue-950 disabled:bg-gray-400 mr-3"
+    // Form Submission Handler
+    const handleSubmit = (event) => {
+        // preventing submission
+        event.preventDefault();
+
+        // item object or whatever
+        const item = {
+            name,
+            quantity,
+            category,
+          };
+        
+        console.log(item);
+
+        
+
+        alert(`${quantity} ${name} from ${category} added to list!`);
+    };
+
 return(
-    <div className="bg-white text-black ">
-        <p className="text-orange-900">Quantity: {quantity}</p>
-        <button className={btnStyle} disabled={btnDisabled} onClick={decrement}>-</button>
-        <button className={btnStyle} onClick={increment}>+</button>
+    <div >
+        <form >
+            <input 
+            type = "text"
+            value = {name}
+            placeholder="Item Name"
+            onChange = {(e) => setName(e.target.value)}
+            required
+            />
+
+            <p>Quantity: {quantity}</p>
+            <button 
+            className="text-black bg-slate-400 w-6 m-4" 
+            disabled={btnDisabled} 
+            onClick={decrement}>-</button>
+
+            <button 
+            className="text-black bg-slate-400 w-6 m-4" 
+            onClick={increment}>+</button>
+
+            <select>
+                <option value="produce">Produce</option>
+                <option value="dairy">Dairy</option>
+                <option value="bakery">Bakery</option>
+                <option value="meat">Meat</option>
+                <option value="frozenFoods">Frozen Foods</option>
+                <option value="cannedGoods">Canned Goods</option>
+                <option value="dryGoods">Dry Goods</option>
+                <option value="beverages">Beverages</option>
+                <option value="snacks">Snacks</option>
+                <option value="household">Household</option>
+                <option value="other">Other</option>
+            </select>
+
+            <button onClick={handleSubmit}>Submit</button>
+        </form>
+
     </div>
 );
 };
